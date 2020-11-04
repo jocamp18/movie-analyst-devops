2b5638766e06c1247ed48ce98037999ce153a8c0
Juan Diego Ocampo García <jocamp18@eafit.edu.co>
2020-11-04 23:07:32 +0000

    Front end configuration script for ui deployment

--- scripts/fe-config.sh
+++ scripts/fe-config.sh
@@ -2,3 +2,12 @@
 
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 systemctl restart sshd
+
+dnf install git npm vim -y
+cd ~/
+git clone https://github.com/jocamp18/movie-analyst-ui.git
+cd movie-analyst-ui
+npm install
+sudo npm install -g pm2
+pm2 start ecosystem.config.js
+crontab /vagrant/config-files/fe-cron

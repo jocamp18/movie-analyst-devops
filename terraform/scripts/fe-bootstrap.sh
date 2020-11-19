5daf10392e821b33d9072dcc4191d7f4006fb611
Juan Diego Ocampo García <jocamp18@eafit.edu.co>
2020-11-19 10:21:06 -0500

    Changing tags

--- terraform/scripts/fe-bootstrap.sh
+++ terraform/scripts/fe-bootstrap.sh
@@ -3,7 +3,7 @@
 yum install npm vim git -y
 amazon-linux-extras install ansible2 -y
 
-git clone -b feat-terraform https://github.com/jocamp18/movie-analyst-devops
+git clone -b v2.0 https://github.com/jocamp18/movie-analyst-devops
 cd movie-analyst-devops/ansible
 
 ansible-playbook -i inventory/ -e "ansible_group=uife os_version=amz" site.yml
